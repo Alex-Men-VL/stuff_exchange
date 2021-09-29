3134c1b5ba91020a5d5509c3cdd9949040ff619c
Aleksandr <menshikov.a.v@icloud.com>
2021-09-30 01:17:00 +0300

    Разбил код на модули и добавил немного функционала

---
+++ add_stuff.py
@@ -0,0 +1,77 @@
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from dotenv import load_dotenv
+
+
+class AddStuff(StatesGroup):
+    waiting_for_photo = State()
+    waiting_for_description = State()
+
+
+async def add_stuff(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.add('Главное меню')
+    await message.answer('Отправьте фото вещи, которую хотите обменять',
+                         reply_markup=keyboard)
+    await AddStuff.waiting_for_photo.set()
+
+
+async def load_stuff_photo(message: types.Message, state: FSMContext):
+    if message.content_type != 'photo':
+        await message.answer('Отправьте фото вашей вещи.')
+        return
+
+    photo_id = message.photo[-1].file_id
+    # TODO: добавление id объявления пользователю
+
+    load_dotenv()
+    bot = Bot(token=os.getenv('TG_TOKEN'))
+
+    photo_file = await bot.get_file(photo_id)
+    photo_name, photo_extension = os.path.splitext(photo_file.file_path)
+    downloaded_photo = await bot.download_file(photo_file.file_path)
+
+    src = f"data/" \
+          f"{message.from_user.id}/{photo_name.split('/')[-1]}{photo_extension}"
+    with open(src, 'wb') as file:
+        file.write(downloaded_photo.getvalue())
+
+    await state.update_data(photo=photo_file)
+    await state.update_data(photo_name=photo_name.split('/')[-1] +
+                            photo_extension)
+
+    await AddStuff.waiting_for_description.set()
+    await message.answer('Теперь введите описание.')
+
+
+async def get_stuff_description(message: types.Message, state: FSMContext):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.add('Добавить вещь')
+
+    if len(os.listdir(f'data/{message.from_user.id}')):
+        keyboard.add('Найти вещь')
+
+    await state.update_data(stuff_desription=message.text)
+    stuff = await state.get_data()
+
+    print(f"\nИзображение: {stuff['photo']}"
+          f"\nОписание: {stuff['stuff_desription']}")
+
+    await message.answer('Объявление добавлено. Вы находитесь в главном '
+                         'меню.', reply_markup=keyboard)
+    await state.finish()
+
+
+def register_handlers_stuff(dp: Dispatcher):
+    dp.register_message_handler(add_stuff, Text(equals='Добавить вещь'),
+                                state="*")
+    dp.register_message_handler(load_stuff_photo,
+                                state=AddStuff.waiting_for_photo,
+                                content_types='photo')
+    dp.register_message_handler(get_stuff_description,
+                                state=AddStuff.waiting_for_description,
+                                content_types=types.ContentTypes.TEXT)
