bcbaaafbb79828094b5e36f8cbc4d243d0ab87cd
Aleksandr <menshikov.a.v@icloud.com>
2021-10-01 16:58:34 +0300

    Добавил функцию по отправке пушей

--- handlers/find_stuff.py
+++ handlers/find_stuff.py
@@ -10,15 +10,29 @@ from models import Stuff, User
 
 
 def get_random_stuff(message):
-    ads = Stuff.select()
-    ad = choice([ad for ad in ads if (
-        ad.owner.telegram_id != message.from_user.id
+    stuffs = Stuff.select()
+    stuff = choice([stuff for stuff in stuffs if (
+            stuff.owner.telegram_id != message.from_user.id
     )])  # добавить проверку, что нет лайка и дизлайка
 
-    stuff_owner_id = ad.owner.telegram_id
-    stuff_photo = InputFile(f'data/{stuff_owner_id}/{ad.image_path}')
-    stuff_description = ad.description
-    return stuff_owner_id, stuff_photo, stuff_description
+    stuff_owner_id = stuff.owner.telegram_id
+    stuff_photo = InputFile(f'data/{stuff_owner_id}/{stuff.image_path}')
+    return stuff_owner_id, stuff_photo, stuff
+
+
+async def send_match(bot, user_id, stuff):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.add('Главное меню')
+    await bot.send_photo(chat_id=user_id, photo='media/match.png',
+                         reply_markup=keyboard)
+    stuff_owner_id = stuff.owner.telegram_id
+    stuff_photo = InputFile(f'data/{stuff_owner_id}/{stuff.image_path}')
+    await bot.send_photo(chat_id=user_id, photo=stuff_photo,
+                         caption=stuff.description)
+
+    await bot.send_message(chat_id=user_id,
+                           text='Ссылка на пользователя: tg://user?id'
+                                f'={stuff_owner_id}\n')
 
 
 async def find_stuff(message: types.Message):
@@ -31,14 +45,19 @@ async def find_stuff(message: types.Message):
     load_dotenv()
     bot = Bot(token=os.getenv('TG_TOKEN'))
 
-    stuff_owner_id, stuff_photo, stuff_description = get_random_stuff(message)
+    stuff_owner_id, stuff_photo, stuff = get_random_stuff(message)
     await bot.send_photo(chat_id=message.from_user.id, photo=stuff_photo,
-                         caption=stuff_description, reply_markup=keyboard)
+                         caption=stuff.description, reply_markup=keyboard)
 
     if message.text == emoji.emojize(':thumbs_up:'):
-        print('Like')
         # добавляем like в бд к вещи
-        # вызов функции для проверки взаимности лайка
+        print('Like')
+        ''' Проверка взаимности лайка
+        try:
+            users_stuff = вещь пользователя (меня), которая понравилась тебе
+            send_match(bot, message.from_user.id, stuff)
+            send_match(bot, stuff_owner_id, users_stuff)
+        '''
 
 
 def register_handlers_ads(dp: Dispatcher):
